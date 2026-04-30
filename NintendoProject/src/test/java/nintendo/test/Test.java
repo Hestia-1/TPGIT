2cbe46a30549526ecb0cfa1e191bfda52f0667c0
Mathieu Legrand <mathieu.legrand78@gmail.com>
2026-04-30 12:28:31 +0200

    creation clien et boutioque

--- NintendoProject/src/test/java/nintendo/test/Test.java
+++ NintendoProject/src/test/java/nintendo/test/Test.java
@@ -1,10 +1,16 @@
 package nintendo.test;
 
+import nintendo.model.Adresse;
+import nintendo.model.Boutique;
+import nintendo.model.Client;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Boutique boutique = new Boutique("Le Canard Enchanté", new Adresse(3, "Avenue", "Cuac"));
+		Client client1 = new Ordinateur("Donald","Canard");
+		Client client2 = new Ordinateur("Jack","Sparrow");
 	}
 
 }
