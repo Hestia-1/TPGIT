80180b1aaa840a6efb06b6a4f1c0689dc503d63f
Mathieu Legrand <mathieu.legrand78@gmail.com>
2026-04-30 12:30:19 +0200

    legere modif

--- NintendoProject/src/test/java/nintendo/test/Test.java
+++ NintendoProject/src/test/java/nintendo/test/Test.java
@@ -9,8 +9,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Boutique boutique = new Boutique("Le Canard Enchanté", new Adresse(3, "Avenue", "Cuac"));
-		Client client1 = new Ordinateur("Donald","Canard");
-		Client client2 = new Ordinateur("Jack","Sparrow");
+		Client client1 = new Client("Donald","Canard");
+		Client client2 = new Client("Jack","Sparrow");
 	}
 
 }
